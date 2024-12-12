c42eea671ef8ef685e4541fd0fd5ff69a40d16d9
ashishk15678 <147980956+ashishk15678@users.noreply.github.com>
2024-12-12 22:28:13 +0530

    FEAT : some changes
    notjing big

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,37 @@
 import { BsLightningFill } from "react-icons/bs";
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 const poppins = Poppins({ subsets: ["latin"], weight: "800", style: "normal" });
 export default function App() {
   return (
     <>
-      <div className="w-full h-full">
-        <div className="p-8 absolute z-99">
-          <BsLightningFill size={25} className="animate-bolt" />
-        </div>
-        <div className="w-[100dvh] h-[100dvh] p-0 md:p-12">
-          <div className="border-b-4 border-r-4 border-violet-500 shadow-md shadow-violet-500 p-4 rounded-xl">
-            <p className={"text-4xl font-sans  text-violet-800 font-extrabold"}>
-              Meet the team
-            </p>
-          </div>
-          <div className="w-full h-full grid place-items-center max-w-screen">
-            <div>
-              <div className=""></div>
-              <div className="w-[400px] h-[100px] bg-violet-700 rounded-2xl shadow-2xl shadow-violet-500  "></div>
-            </div>
-          </div>
-        </div>
+      <div className="w-[100dvh] h-[100dvh] flex items-center justify-center">
+        <p className="text-xl">
+          Please have patience , still building
+          <br />
+          Till then visit -{" "}
+          <Link href="https://ashish.services">ashish.services</Link>
+        </p>
+        <p>Else have a chat with builders</p>
+        <p>
+          <Link href={"https://instagram.com/s.dwiz"}>Dwiz Vrat Sharma</Link>
+        </p>
+        <p>
+          <Link href={"https://instagram.com/ashishkumar15678"}>
+            Ashish Kumar
+          </Link>
+        </p>
+        <p>
+          <Link href={"https://instagram.com/vikrant_maurya14"}>
+            Vikrant Maurya
+          </Link>
+        </p>
+        <p>
+          <Link href={"https://github.com/pragati-30"}>Pragati</Link>
+        </p>
       </div>
+
+      <div>Raise issues at help.gdg@ashish.services</div>
     </>
   );
 }
